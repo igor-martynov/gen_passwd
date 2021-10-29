ef24882fa6c0597798cd0cd60b5c57f39d6f85dd
igor martynov <phx.planewalker@gmail.com>
2021-10-29 15:59:37 +0300

    Update gen_passwd.py

--- gen_passwd.py
+++ gen_passwd.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# 2021-10-26
-
-__version__ = "0.1.1"
+__version__ = "0.3.0"
 __author__ = "Igor Martynov (phx.planewalker@gmail.com)"
 
 
@@ -11,6 +9,7 @@ __author__ = "Igor Martynov (phx.planewalker@gmail.com)"
 Python3 required.
 
 See print_help() for usage info"""
+
 
 # basic imports
 import os
@@ -21,26 +20,30 @@ import random
 import string
 
 
-def gen_random_str(length):
-	return ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for x in range(length))
+def gen_random_str(length, choose_from = string.ascii_uppercase + string.ascii_lowercase + string.digits):
+	return ''.join(random.choice(choose_from) for x in range(length))
 
 
 def print_help():
 	print()
-	print(f"Usage: {os.path.abspath(__file__)} [-C number of password lines] [-L lenght of password]")
+	print(f"Usage: {os.path.abspath(__file__)} [-C count of password lines] [-L lenght of password] [options]")
+	print("		options:")
+	print("		--only-digits: use only digits in passwd")
+	print("		--help | -h: print this help ")
+	print()
 	print(f"Example: {os.path.abspath(__file__)} -C 10 -L 12 		- will generate 10 passwords, 12 chars each")
 	print()
 
 
 if __name__ == "__main__":
-	# defaults
-	LENGTH = 10
-	LINE_COUNT = 10
-	PRINT_EXTRA_SPACES = True
-	
 	# CLI arguments
 	args = sys.argv[1:]
 	
+	# defaults
+	LENGTH = 10
+	LINE_COUNT = 10
+	PRINT_EXTRA_SPACES = True	
+
 	if "--help" in args or "-h" in args:
 		print_help()
 		sys.exit(0)
@@ -58,9 +61,13 @@ if __name__ == "__main__":
 			print(f"ERROR: got error while parsing password line count: {e}")
 			print_help()
 			sys.exit(1)
+	if "--only-digits" in args:
+		CHOOSE_FROM = string.digits
+	else:
+		CHOOSE_FROM = string.ascii_uppercase + string.ascii_lowercase + string.digits
 	
-	# generating here
+	# generating passwords here
 	if PRINT_EXTRA_SPACES: print()
 	for i in range(LINE_COUNT):
-		print(gen_random_str(LENGTH))
+		print(gen_random_str(LENGTH, choose_from = CHOOSE_FROM))
 	if PRINT_EXTRA_SPACES: print()
